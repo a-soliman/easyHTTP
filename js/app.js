929f1d9518d8c64c4462884b5d035f922e79a704
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-18 08:03:06 -0700

    tested the post request

--- js/app.js
+++ js/app.js
@@ -13,12 +13,27 @@ const http = new easyHTTP();
 
 
 // GET single post
-http.get('https://jsonplaceholder.typicode.com/posts/1', ( err, post ) => {
-    //check for err
+// http.get('https://jsonplaceholder.typicode.com/posts/1', ( err, post ) => {
+//     //check for err
+//     if ( err ) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(post);
+//     }
+// });
+
+// POST Request
+const data = {
+    title : 'Custom Post',
+    body: 'Cusom post Body'
+};
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, ( err, response) => {
     if ( err ) {
         console.log(err);
     }
     else {
-        console.log(post);
+        console.log(response);
     }
-});
+})
