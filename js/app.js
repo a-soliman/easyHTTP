1e890c9e746ec3bd844d5d3b85ab8c6cc2a8cbac
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-18 07:48:29 -0700

    used the get request to return a single post

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
 const http = new easyHTTP();
 
 // GET POSTS
-http.get('https://jsonplaceholder.typicode.com/posts', ( err, response )=> {
-    // check for error
+// http.get('https://jsonplaceholder.typicode.com/posts', ( err, response )=> {
+//     // check for error
+//     if ( err ) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(response);
+//     }
+// });
+
+
+// GET single post
+http.get('https://jsonplaceholder.typicode.com/posts/1', ( err, post ) => {
+    //check for err
     if ( err ) {
         console.log(err);
     }
     else {
-        console.log(response);
+        console.log(post);
     }
 });
