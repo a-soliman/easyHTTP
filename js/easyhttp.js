68cd6324f1575fde482f2324cad4b7fa5d0c7c8a
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-18 08:02:52 -0700

    fixed a typo

--- js/easyhttp.js
+++ js/easyhttp.js
@@ -26,8 +26,7 @@ easyHTTP.prototype.post = function( url, data, callback ) {
         callback(null, self.http.responseText);
     };
 
-
-    this.send(JSON.stringify(data));
+    this.http.send(JSON.stringify(data));
 };
 
 // PUT
