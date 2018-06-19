15995b16e0caa26bda6a3ed146dc917d29b39c63
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-19 07:12:17 -0700

    restructured the post method

--- js/easyhttp.js
+++ js/easyhttp.js
@@ -7,6 +7,20 @@ class EasyHTTP {
             .then(data => data)
             .catch(err => err);
     }
+
+    // POST
+    post( url, data ) {
+        return fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data => data)
+        .catch(err => err);
+    }
 }
 
 
