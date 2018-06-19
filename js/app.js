a61187fa5c239641798b697067bf492594b00943
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-19 07:08:15 -0700

    testing the new get request

--- js/app.js
+++ js/app.js
@@ -1,57 +1,50 @@
-const http = new easyHTTP();
+const http = new EasyHTTP();
 
 // GET POSTS
-http.get('https://jsonplaceholder.typicode.com/posts', ( err, response )=> {
-    // check for error
-    if ( err ) {
-        console.log(err);
-    }
-    else {
-        console.log(response);
-    }
-});
+http.get('https://jsonplaceholder.typicode.com/posts')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 
+// // GET single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', ( err, post ) => {
+//     //check for err
+//     if ( err ) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(post);
+//     }
+// });
 
-// GET single post
-http.get('https://jsonplaceholder.typicode.com/posts/1', ( err, post ) => {
-    //check for err
-    if ( err ) {
-        console.log(err);
-    }
-    else {
-        console.log(post);
-    }
-});
+// // POST Request
+// const data = {
+//     title : 'Custom Post',
+//     body: 'Cusom post Body'
+// };
 
-// POST Request
-const data = {
-    title : 'Custom Post',
-    body: 'Cusom post Body'
-};
+// http.post('https://jsonplaceholder.typicode.com/posts', data, ( err, response) => {
+//     if ( err ) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(response);
+//     }
+// });
 
-http.post('https://jsonplaceholder.typicode.com/posts', data, ( err, response) => {
-    if ( err ) {
-        console.log(err);
-    }
-    else {
-        console.log(response);
-    }
-});
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, ( err, response ) => {
+//     if ( err ) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(response);
+//     }
+// });
 
-http.put('https://jsonplaceholder.typicode.com/posts/1', data, ( err, response ) => {
-    if ( err ) {
-        console.log(err);
-    }
-    else {
-        console.log(response);
-    }
-});
-
-http.delete('https://jsonplaceholder.typicode.com/posts/1', ( err, response ) => {
-    if ( err ) {
-        console.log(err);
-    }
-    else {
-        console.log(response);
-    }
-});
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', ( err, response ) => {
+//     if ( err ) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(response);
+//     }
+// });
