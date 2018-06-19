7d43b0d0b51acb7ed6715b1bb3a22db8708209aa
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-19 07:07:48 -0700

    converting to classes

--- js/easyhttp.js
+++ js/easyhttp.js
@@ -1,58 +1,72 @@
-function easyHTTP() {
-    this.http = new XMLHttpRequest();
+class EasyHTTP {
+
+    // GET
+    get( url ) {
+        return fetch(url)
+            .then(res => res.json())
+            .then(data => data)
+            .catch(err => err);
+    }
 }
 
-// GET 
-easyHTTP.prototype.get = function( url, callback ) {
-    this.http.open('GET', url, true);
 
-    const self = this;
-    this.http.onload = function() {
-        if ( self.http.status === 200 ) {
-            callback(null, self.http.responseText);
-        } else {
-            callback('Error: ' + self.http.status);
-        }
-    };
 
-    this.http.send();
-};
-// POST
-easyHTTP.prototype.post = function( url, data, callback ) {
-    this.http.open('POST', url, true);
-    this.http.setRequestHeader('Content-Type', 'application/josn');
-    const self = this;
-    this.http.onload = function() {
-        callback(null, self.http.responseText);
-    };
 
-    this.http.send(JSON.stringify(data));
-};
+// function easyHTTP() {
+//     this.http = new XMLHttpRequest();
+// }
 
-// PUT
-easyHTTP.prototype.put = function( url, data, callback ) {
-    this.http.open('PUT', url, true);
-    this.http.setRequestHeader('Content-Type', 'application/json');
-    const self = this;
-    this.http.onload = function() {
-        callback(null, self.http.responseText);
-    };
+// // GET 
+// easyHTTP.prototype.get = function( url, callback ) {
+//     this.http.open('GET', url, true);
 
-    this.http.send(JSON.stringify(data));
-};
+//     const self = this;
+//     this.http.onload = function() {
+//         if ( self.http.status === 200 ) {
+//             callback(null, self.http.responseText);
+//         } else {
+//             callback('Error: ' + self.http.status);
+//         }
+//     };
 
-// DELETE
-easyHTTP.prototype.delete = function( url, callback ) {
-    this.http.open('DELETE', url, true);
-    const self = this;
-    this.http.onload = function() {
-        if ( self.http.status == 200 ) {
-            callback(null, self.http.responseText);
-        }
-        else {
-            callback('Error: ' + self.http.status);
-        }
-    };
+//     this.http.send();
+// };
+// // POST
+// easyHTTP.prototype.post = function( url, data, callback ) {
+//     this.http.open('POST', url, true);
+//     this.http.setRequestHeader('Content-Type', 'application/josn');
+//     const self = this;
+//     this.http.onload = function() {
+//         callback(null, self.http.responseText);
+//     };
 
-    this.http.send();
-};
+//     this.http.send(JSON.stringify(data));
+// };
+
+// // PUT
+// easyHTTP.prototype.put = function( url, data, callback ) {
+//     this.http.open('PUT', url, true);
+//     this.http.setRequestHeader('Content-Type', 'application/json');
+//     const self = this;
+//     this.http.onload = function() {
+//         callback(null, self.http.responseText);
+//     };
+
+//     this.http.send(JSON.stringify(data));
+// };
+
+// // DELETE
+// easyHTTP.prototype.delete = function( url, callback ) {
+//     this.http.open('DELETE', url, true);
+//     const self = this;
+//     this.http.onload = function() {
+//         if ( self.http.status == 200 ) {
+//             callback(null, self.http.responseText);
+//         }
+//         else {
+//             callback('Error: ' + self.http.status);
+//         }
+//     };
+
+//     this.http.send();
+// };
