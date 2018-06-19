c2db30627e10b1f25ce66b6e1a1acf4c41819823
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-19 07:20:57 -0700

    added descriptive comments

--- js/app.js
+++ js/app.js
@@ -5,22 +5,23 @@ http.get('https://jsonplaceholder.typicode.com/posts')
     .then(data => console.log(data))
     .catch(err => console.log(err));
 
-
-// POST Request
+// TESTING DATA
 const data = {
     title : 'Custom Post',
     body: 'Cusom post Body'
 };
 
+// POST Request
 http.post('https://jsonplaceholder.typicode.com/posts', data)
     .then(res => console.log(res))
     .catch(err => console.log(err));
 
+// PUT Request
 http.put('https://jsonplaceholder.typicode.com/posts/1', data)
     .then(data => console.log(data))
     .catch(err => console.log(err));
 
-
+// DELETE Request
 http.delete('https://jsonplaceholder.typicode.com/posts/1')
     .then(data => console.log(data))
     .catch(err => console.log(err));
--- js/easyhttp.js
+++ js/easyhttp.js
@@ -46,34 +46,3 @@ class EasyHTTP {
         .catch(err => err);
     }
 }
-
-
-
-
-// // PUT
-// easyHTTP.prototype.put = function( url, data, callback ) {
-//     this.http.open('PUT', url, true);
-//     this.http.setRequestHeader('Content-Type', 'application/json');
-//     const self = this;
-//     this.http.onload = function() {
-//         callback(null, self.http.responseText);
-//     };
-
-//     this.http.send(JSON.stringify(data));
-// };
-
-// // DELETE
-// easyHTTP.prototype.delete = function( url, callback ) {
-//     this.http.open('DELETE', url, true);
-//     const self = this;
-//     this.http.onload = function() {
-//         if ( self.http.status == 200 ) {
-//             callback(null, self.http.responseText);
-//         }
-//         else {
-//             callback('Error: ' + self.http.status);
-//         }
-//     };
-
-//     this.http.send();
-// };
