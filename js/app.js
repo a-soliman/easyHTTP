ae23d91f62f153402d7057d93fdc7254c2afbfb5
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-19 07:12:29 -0700

    tested the new post request

--- js/app.js
+++ js/app.js
@@ -5,31 +5,16 @@ http.get('https://jsonplaceholder.typicode.com/posts')
     .then(data => console.log(data))
     .catch(err => console.log(err));
 
-// // GET single post
-// http.get('https://jsonplaceholder.typicode.com/posts/1', ( err, post ) => {
-//     //check for err
-//     if ( err ) {
-//         console.log(err);
-//     }
-//     else {
-//         console.log(post);
-//     }
-// });
 
-// // POST Request
-// const data = {
-//     title : 'Custom Post',
-//     body: 'Cusom post Body'
-// };
+// POST Request
+const data = {
+    title : 'Custom Post',
+    body: 'Cusom post Body'
+};
 
-// http.post('https://jsonplaceholder.typicode.com/posts', data, ( err, response) => {
-//     if ( err ) {
-//         console.log(err);
-//     }
-//     else {
-//         console.log(response);
-//     }
-// });
+http.post('https://jsonplaceholder.typicode.com/posts', data)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
 
 // http.put('https://jsonplaceholder.typicode.com/posts/1', data, ( err, response ) => {
 //     if ( err ) {
